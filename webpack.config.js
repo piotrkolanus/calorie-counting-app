b3c0225ec3b4084c5db4ed576e917d23e6055271
Piotr <piotr.kolanus@gmail.com>
2019-03-22 22:16:27 +0100

    working development environment setup

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const extractPlugin = new MiniCssExtractPlugin({
+  filename: "main.css",
+  publicPath: "/dist",
+  chunkFilename: "[id].css"
+});
+
+module.exports = {
+  entry: "./src/js/app.js",
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist/"
+  },
+  watch: true,
+  devServer: {
+    writeToDisk: true,
+    contentBase: "./src",
+    compress: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.s?[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: "css-loader", options: { url: false, sourceMap: true } },
+          { loader: "sass-loader", options: { sourceMap: true } }
+        ]
+      }
+    ]
+  },
+  plugins: [extractPlugin]
+};
