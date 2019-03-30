f9c0a09d924676d8f69aff8bfd7996180ef8dff7
Piotr <piotr.kolanus@gmail.com>
2019-03-30 19:03:04 +0100

    imputs binding to model

--- src/js/Update.js
+++ src/js/Update.js
@@ -1,4 +1,45 @@
+const MESSAGES = {
+  SHOW_FORM: "SHOW_FORM",
+  MEAL_INPUT: "MEAL_INPUT",
+  CALORIES_INPUT: "CALORIES_INPUT"
+};
+
+export function showFormMsg(showForm) {
+  return {
+    type: MESSAGES.SHOW_FORM,
+    showForm
+  };
+}
+
+export function mealInputMsg(description) {
+  return {
+    type: MESSAGES.MEAL_INPUT,
+    description
+  };
+}
+
+export function caloriesInputMsg(calories) {
+  return {
+    type: MESSAGES.CALORIES_INPUT,
+    calories
+  };
+}
+
 function update(msg, model) {
+  switch (msg.type) {
+    case MESSAGES.SHOW_FORM: {
+      const { showForm } = msg;
+      return { ...model, showForm: showForm, description: "", calories: 0 };
+    }
+    case MESSAGES.MEAL_INPUT: {
+      const { description } = msg;
+      return { ...model, description };
+    }
+    case MESSAGES.CALORIES_INPUT: {
+      const calories = parseInt(msg.calories) || 0;
+      return { ...model, calories };
+    }
+  }
   return model;
 }
 
--- src/js/View.js
+++ src/js/View.js
@@ -1,19 +1,32 @@
 import hh from "hyperscript-helpers";
 import { h } from "virtual-dom";
+import { showFormMsg, mealInputMsg, caloriesInputMsg } from "./Update";
 
 const { pre, div, h1, form, fieldset, button, header, label, input } = hh(h);
 
-function fieldSet(labelText, inputValue) {
+function fieldSet(labelText, inputValue, oninput) {
   return fieldset({ className: "form__fieldset" }, [
     label({ className: "form__label" }, labelText),
-    input({ className: "form__input", type: "text", value: inputValue })
+    input({
+      className: "form__input",
+      type: "text",
+      value: inputValue,
+      oninput
+    })
   ]);
 }
 
 function buttonSet(dispatch) {
   return div({ className: "form-button-box" }, [
     button({ className: "form-button-box__button", type: "submit" }, "Save"),
-    button({ className: "form-button-box__button", type: "button" }, "Cancel")
+    button(
+      {
+        className: "form-button-box__button",
+        type: "button",
+        onclick: () => dispatch(showFormMsg(false))
+      },
+      "Cancel"
+    )
   ]);
 }
 
@@ -22,22 +35,34 @@ function formView(dispatch, model) {
   if (showForm) {
     return div({ className: "form-container" }, [
       form({ className: "form-box" }, [
-        fieldSet("Meal", description),
-        fieldSet("Calories", calories || ""),
+        fieldSet("Meal", description, e =>
+          dispatch(mealInputMsg(e.target.value))
+        ),
+        fieldSet("Calories", calories || "", e =>
+          dispatch(caloriesInputMsg(e.target.value))
+        ),
         buttonSet(dispatch)
       ])
     ]);
   }
 
   return div({ className: "toggle-form" }, [
-    button({ className: "toggle-form__button", type: "button" }, "Add meal")
+    button(
+      {
+        className: "toggle-form__button",
+        onclick: () => dispatch(showFormMsg(true)),
+        type: "button"
+      },
+      "Add meal"
+    )
   ]);
 }
 
 function view(dispatch, model) {
   return div({ className: "container" }, [
     header({ className: "header" }, [h1({}, "Calorie Counter")]),
-    formView(dispatch, model)
+    formView(dispatch, model),
+    pre(JSON.stringify(model, null, 2))
   ]);
 }
 
